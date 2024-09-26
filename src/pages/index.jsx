a8c03fee33628dbedbf1439d7924afb34931d587
shuhrat4 <xudoyberdiyevshuhrat5@gmail.com>
2024-09-27 03:49:09 +0500

    start

---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+import Register from './Register'
+import Login from './Login'
+import Home from './Home'
+import Profile from './Profile/Profile'
+
+export {Register, Login, Home, Profile}
---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+
+function index() {
+    return (
+        <Routes>
+            <Routes path='/' element />
+        </Routes>
+    )
+}
+
+export default index
